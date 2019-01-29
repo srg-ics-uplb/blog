e369a771a4ca2a6dc78671cd3332aa5b9d3612b4
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-01-29 16:20:17 +0800

    edits

--- content/jach_001.rst
+++ content/jach_001.rst
@@ -2,7 +2,7 @@ Welcome!
 ##########
 
 :date: 2019-1-29
-:category: Introfuction
+:category: Introduction
 :author: Joseph Anthony C. Hermocilla
 
 Hello World from Pelican!
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'SRG'
-SITENAME = u'Systems Research Group Blog'
+SITENAME = u'Thoughts of a Packet'
 SITEURL = ''
 
 PATH = 'content'
@@ -22,14 +22,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
