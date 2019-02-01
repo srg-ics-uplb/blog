6a284516693ccfad5c83ca0bbbcd997f053d4329
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-02-01 21:23:14 +0800

    edits

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'SRG'
-SITENAME = u'Thoughts of a Packet'
+SITENAME = u'Thoughts of Lost Packets'
 SITEURL = u'https://srg-ics-uplb.github.io/blog'
 
 PATH = 'content'
